443b2976af68ec6a8ffc4ebc6de2057ffbde0117
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2024-10-28 10:49:02 +0100

    Update patient criteria check

--- patient-search-backend/api.py
+++ patient-search-backend/api.py
@@ -117,7 +117,9 @@ async def patient_search(request: Request):
     return { 'output': outputs}
 
 ############################################################################
-def ask_to_llm(prompt, system_message="You are a helpful assistant and a medical expert"):
+
+
+def ask_to_llm(prompt, system_message="You are a healthcare assistant and a medical expert on information extraction"):
         data = {
         "messages": [
             {
@@ -149,52 +151,142 @@ def ask_to_llm(prompt, system_message="You are a helpful assistant and a medical
             return completion_text
         else:
             raise ValueError(f"Request problem. Status Code: {response.status_code}")
-        
+
+
 @app.post('/criteria_check')
 async def criteria_check(request: Request):
     request_data = await request.json()
     criteria = request_data['criteria']
-    #query = request_data['query']
-    #patient_search_output = cache.get(query, [])
-    # patient_search_output = await patient_search(request)
+
     patient_search_output = outputs_list[-1] if outputs_list else []
     i_criteria = []
+
+    few_shot_examples = '''
+   
+    Eligibility Criteria:
+    - Age 40 to 60
+    - Female
+    - History of  urinary tract infections
+ 
+    Clinical Text:
+    ```
+    Medical History:
+    A 30-year-old woman presented with recurrent urinary tract infections, occasional left lumbar pain, and continuous dribbling of urine. Physical examination revealed two urethral openings, one below the other in the sagittal plane, below the clitoris. Her renal function tests were normal.
+ 
+    Diagnostic Imaging:
+    Ultrasound scan of the abdomen showed gross hydroureteronephrosis of the left kidney extending up to the lower ureter, and the urinary bladder had a very large capacity with irregular lobulated contours. Intravenous pyelogram (IVP) showed a large capacity bladder with left gross hydroureteronephrosis and severely tortuous left ureter suggestive of left obstructive megaureter. Micturating cystourethrogram also revealed a very large capacity bladder. The magnetic resonance imaging of the spine was normal.
+ 
+    Cystoscopy:
+    Cystoscopy showed a double urethra, one above the other in the sagittal plane. The posterior urethra was in the orthotopic position and was opening into the large capacity bladder. The anterior urethra was present just posterior to the clitoris. The internal opening of the accessory (anterior) urethra was located about 2 cm cranial to the bladder neck on the anterior wall of the urinary bladder. Green arrow in shows the vaginal opening.
+ 
+    Treatment:
+    Excision of the accessory anterior urethra with reduction cystoplasty and left ureteric tailoring with reimplantation was performed in a single setting. The excision of the accessory urethra was performed with the child in the lithotomy position through the abdominoperineal route. Since the excised urethra was posterior to the clitoris, the nerve supply to the clitoris was not disrupted. The orthotopic urethra and the bladder neck were also safeguarded during the dissection. The resected accessory urethra had transitional cell lining with thin, poorly muscular wall on the histopathology.
+ 
+    Follow-Up:
+    The patient was followed up with 3 monthly urine microscopy and culture, RFT, ultrasound scan of the abdomen at 6 months, and IVP at the end of 1 year of surgery. Since these were normal, she was followed up with only RFT and ultrasound scan of the abdomen annually for the past 5 years and is doing well.
+    ```
+    Response:
+    Eligible because the patient is a 30-year-old woman with recurrent urinary tract infections which meets all criteria.
+   
+    Clinical Text:
+    ```
+    Discharge Summary:
+ 
+    Patient: 81-year-old man with metastatic melanoma, remote history of mantle cell lymphoma, mild oral lichen planus, gout, gastroesophageal reflux disease, chronic bronchitis, benign prostatic hyperplasia, and depression.
+ 
+    History: The patient failed first-line course of BRAF-targeted therapy with dabrafenib and trametinib. In November 2018, he started with second-line pembrolizumab, which was discontinued in May 2019 due to the serial progression of disease on imaging. Within 6 weeks of discontinuation of therapy, the patient presented with a generalized skin eruption, composed of multiple scaly erythematous papules and plaques, some with central hyperkeratotic crust, in a symmetric distribution on the arms, chest, back, abdomen, and upper legs. No other medications were introduced prior to development of the cutaneous eruption.
+ 
+    Treatment: The patient was initially treated with high-dose potency topical steroids and rapidly moved to prednisone 1 mg/kg when he did not have an adequate response. He had a mild initial good response, but the lesions flared on the taper of steroids at the same time as a grade 2 immune-related colitis. On increase of corticosteroids, the patient had a resolution of his colitis and stability of his lesions; prednisone was tapered for a total of 2 months of treatment. He was subsequently serially treated with methotrexate for 4 weeks, followed by mycophenolate mofetil for 4 weeks, and then cyclosporine for 4 weeks, with a mild response to therapy at best. After consultation in a multidisciplinary immune-toxicity team, it was decided that NBUVB phototherapy could be considered. The patient received 17 total sessions, which induced a significant healing of his lesions and the resolution of the pruritus and skin discomfort.
+ 
+    Outcome: Subsequent to his immune-toxicities, a significant systemic antitumor response was noted on his staging scans, which are currently ongoing over 12 months after cessation of PD-1 therapy.
+ 
+    Impression: The patient's cutaneous eruption was diagnosed as a lichenoid drug reaction, which was effectively treated with NBUVB phototherapy. He had a mild response to other systemic treatments. The patient was discharged in stable condition with recommendations for follow-up with his oncologist.
+    ```
+    Response:
+    Not Eligible because age is 81 years old which is outside the range of 40 to 60.
+   
+    Clinical Text:
+    ```
+    Electronic Health Record
+ 
+    Patient Name: [De-identified]
+    Age: [AGE]
+    Medical Record Number: [De-identified]
+ 
+    Discharge Summary
+ 
+    Admission Date: [Date]
+    Discharge Date: [Date]
+ 
+    Admitting Diagnosis: Abdominal pain and a palpable mass in the right hemiabdomen
+ 
+    Hospital Course:
+    The patient presented with abdominal pain and a palpable mass in the right hemiabdomen. The physical examination was otherwise normal. Abdominal CT revealed an oval, sharply marginated, heterogeneous, mass-like lesion in the wall of the right colon that was 110 × 95 × 90 mm in size. A postcontrast CT examination of the tumour showed heterogeneous enhancement. On MRI, the tumour displayed heterogeneous signal intensity on T2-weighted images with irregular areas of hyperintensity.
+    Surgery was performed and a well-marginated mass that invaded the wall of the caecum and small bowel mesentery near the terminal ileum was found intraoperatively. Thus, a right hemicolectomy with partial resection of the terminal ileum was performed, with latero-lateral ileocolonic anastomosis. Histological examination revealed mesenchymal proliferation with uniform bland spindle-shaped cells in long ‘sweeping’ fascicles or loose, vague storiform arrays. Medium-sized stromal blood vessels with some perivascular hyalinization and smaller vessels often elongated and compressed between lesional fascicles were found. Further immunohistochemical examination showed nuclear immunohistochemical identification of beta-catenin and the diagnosis of DTF was made.
+ 
+    Discharge Diagnosis: DTF (Desmoid-type fibromatosis)
+ 
+    Discharge Condition: Stable
+ 
+    Summary:
+    The patient presented with abdominal pain and a palpable mass in the right hemiabdomen. The diagnosis of DTF was made after surgery and histological examination. The patient underwent a right hemicolectomy with partial resection of the terminal ileum, and latero-lateral ileocolonic anastomosis was performed. At 1 year after surgery, the patient remains disease-free and has no signs of disease recurrence. The patient was discharged in stable condition.
+ 
+    Recommendations:
+    The patient should continue regular follow-up with their healthcare provider in order to monitor their condition and detect any potential disease recurrence.
+ 
+    Follow-up:
+    The patient will have regular follow-up appointments with their healthcare provider.
+    ```
+    Response
+    Insufficient information because age is not mentioned in the Clinical Text.
+    '''
+
     for document in patient_search_output:
-        prompt = f'''Given the following clinical text:
+        prompt = f'''{few_shot_examples}
+       
+        Evaluate the patient's eligibility based on the clinical text and the provided inclusion criteria.
+       
+        clinical text:
         ```
         {document['text']}
         ```
-        and given the following eligibility inclusion criteria for a clinical trial:
+        Inclusion Criteria:
         ```
         {criteria}
         ```
-        
-        Please carefully evaluate the patient information, paying close attention to any specific inclusion criteria, especially numeric values (e.g., age, weight, lab results, etc.).
-
-        - If the required information is available, use the exact values in your response.
+       
+        Use the following guidelines to determine eligibility:
+        - If the required information is available in clinical text, use the exact values in your response.
         - If any single criterion is not met, respond with "Not Eligible because ..." and state the reason.
-        - In any case of unmentioned or unavailable information or values, do not respond with "Eligible because ..." under any circumstances.
         - If the information is missing, ambiguous, inferred, or unclear in the clinical text, respond with "Insufficient information because ..." and explain that the necessary information is not explicitly stated.
-        - Do not use placeholders like "[REDACTED]" If exact values are not provided in the clinical text, do not make assumptions or inferences.
+        - Note that placeholders like "[REDACTED]", "[AGE]", "[DOB]" or any combinations like "Patient Age: [REDACTED]" are not considered valid age information and should not be used.
         - If a criterion is inferred (e.g., the patient's age is inferred but not explicitly mentioned), respond with "Insufficient information because ..." and explain that the information is not explicitly stated.
-        - If any placeholders like "[REDACTED]" is used in the clinical text instead of the exact numeric values such as age, weight, etc, respond with "Insufficient information because ...".
         - If any numeric values was not mentioned or mentioned as "[REDACTED]" (e.g., date of birth: [REDACTED]), respond with "Insufficient information because ...".
-
-        Respond only in the following format: 
-        "Eligible because ..." 
-        or 
-        "Not Eligible because ..." 
-        or 
-        "Insufficient information because ...".
-        
+        - Only if all criteria are met exactly, respond with "Eligible because ...".
         '''
+
         answer = ask_to_llm(prompt)
         i_criteria.append(answer)
-    # i_criteria = ["yes", "no", "yes"]
-    in_criteria = []
-    for document in i_criteria:
-        output_dict = {}
-        output_dict['text'] = document
-        in_criteria.append(output_dict)
-    
-    return {'criteria': in_criteria}
+
+    comparison_results = []
+    for response in i_criteria:
+        reformat_prompt = f'''
+        Organize the following eligibility response into a concise, structured format.
+ 
+        Eligibility Response
+        ```
+        {response}
+        ```
+ 
+        Respond in this format:
+        [Eligibility Status] because: [Concise reason]
+       
+        Eligibility Status: Eligible, Not Eligible, or Insufficient information
+        Respond only in the format above without extra text.
+        '''
+
+        # Second step: Ask LLM to perform the comparison and return eligibility result
+        comparison_result = ask_to_llm(reformat_prompt)
+        comparison_results.append(comparison_result)
+
+    return {'criteria': comparison_results}
